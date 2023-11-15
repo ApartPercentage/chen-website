e2a54011d6819b34e859cc2ff993ee27284f2ff3
ApartPercentage <67103570+ApartPercentage@users.noreply.github.com>
2023-11-15 13:09:53 +0000

    HTML & CSS

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__) # created Flask application
 
 @app.route("/") #register a route to the application
 def hello_world():
-  return "Hello World"
+  return render_template('home.html')
 
 if __name__ == "__main__":  #check if running app.py as a script, then start the app using Run
   app.run(host = '0.0.0.0', debug = True) #0000 runs locally
