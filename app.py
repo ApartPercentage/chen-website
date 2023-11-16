97b6961775cfadd22bb1d0fe050cd8d6a892b563
ApartPercentage <67103570+ApartPercentage@users.noreply.github.com>
2023-11-16 11:08:30 +0000

    Added route for projects

--- app.py
+++ app.py
@@ -23,5 +23,13 @@ def hello_world():
 def list_projects():
   return jsonify(PROJECTS)
 
+@app.route('/project/<int:project_id>')
+def project(project_id):
+    # Add code to retrieve project details based on project_id
+    # For example: project = get_project_by_id(project_id)
+    # Then pass the project details to the template
+    # return render_template('project_detail.html', project=project)
+    return f"Project {project_id} details"
+
 if __name__ == "__main__":  #check if running app.py as a script, then start the app using Run
   app.run(host = '0.0.0.0', debug = True) #0000 runs locally
